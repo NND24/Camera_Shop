b7f8c53ad617863165e76074988819627d319f4c
ngocdatnguyen2404@gmail.com <=>
2023-05-03 15:05:52 +0700

    update

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$mysqli = new mysqli("localhost", "root", "", "camera_shop");
+$mysqli = new mysqli("localhost", "id20562858_gpmcamera", "E$/eM/1KHb{b?D23", "id20562858_camera_shop");
 
 // Check connection
 if ($mysqli->connect_error) {
