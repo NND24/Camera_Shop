0649879a908da15498f87764e87bbcb470bb6c56
ngocdatnguyen2404@gmail.com <=>
2023-05-03 15:21:30 +0700

    update

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$mysqli = new mysqli("localhost", "id20562858_gpmcamera", "E$/eM/1KHb{b?D23", "id20562858_camera_shop");
+$mysqli = new mysqli("sql.freedb.tech", "freedb_gpm_camera", "wk94?FQWZh@r4V$", "freedb_camera_shop");
 
 // Check connection
 if ($mysqli->connect_error) {
--- admin/modules/menu.php
+++ admin/modules/menu.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$mysqli = new mysqli("localhost", "id20562858_gpmcamera", "E$/eM/1KHb{b?D23", "id20562858_camera_shop");
+$mysqli = new mysqli("sql.freedb.tech", "freedb_gpm_camera", "wk94?FQWZh@r4V$", "freedb_camera_shop");
 if (!isset($_SESSION['dangnhap'])) {
     header('Location: login.php');
 }
